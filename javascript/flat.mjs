b9729fc933b53075387b259090c8b8b3303cd3ea
Gabriel Debona <gabriel.debona@gmail.com>
2025-11-28 22:48:02 -0300

    improved flat poc

--- javascript/flat.mjs
+++ javascript/flat.mjs
@@ -1,3 +1,23 @@
 const arr1 = [1, 2, [3, 4]];
-arr1.flat();
-console.log({arr1})
+const ex1 = arr1.flat();
+const ex2 = arr1.flat(1);
+const ex3 = arr1.flat(2);
+const ex4 = arr1.flat(Infinity);
+
+console.log({ex1});
+console.log({ex2});
+console.log({ex3});
+console.log({ex4});
+
+const arr2 = [1, 2, [3, 4, [5, 6]]];
+const test1 = arr2.flat();
+const test2 = arr2.flat(2);
+const test3 = arr2.flat(Infinity);
+
+console.log({test1});
+console.log({test2});
+console.log({test3});
+
+const matrix = [[1,2,3],[4,5,6]];
+const list = matrix.flat();
+console.log(list);
