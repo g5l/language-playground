c7e3ff35b773e5115daf002179b329756ebd4373
Gabriel Debona <gabriel.debona@gmail.com>
2026-01-18 23:05:10 -0300

    more things for objectLiterals

--- javascript/objectLiterals.js
+++ javascript/objectLiterals.js
@@ -1,11 +1,35 @@
 const id = 1;
 const name = 'Alice';
+const surname = 'Silva';
 const age = 2;
 const gender = 'female';
+
+const prefix = name + '-';
 
 const person = {
   id,
   name,
+  surname,
   age,
-  gender
+  gender,
+  fullName() {
+    return `${this.name} ${this.surname}`;
+  },
+  [prefix.toLowerCase() + 'createdAt']: Date.now(),
+  [`${prefix.toLowerCase()}updatedAt`]: Date.now(),
+  
+  get agee() {
+    return this.age;
+  },
+  
+  set setFullName(value) {
+    [this.name, this.surname] = value.split(' ');
+  }
 }
+
+console.log(person);
+console.log({fullName: person.fullName(), name: person.name, surname: person.surname});
+console.log({age: person.agee});
+person.setFullName = 'New Name';
+console.log('New name');
+console.log({fullName: person.fullName(), name: person.name, surname: person.surname});
