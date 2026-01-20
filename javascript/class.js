c13ff794e8b7e0a439f48097e27766d0e3e8c863
Gabriel Debona <gabriel.debona@gmail.com>
2026-01-20 09:52:01 -0300

    Created class POC

---
+++ javascript/class.js
@@ -0,0 +1,62 @@
+import {separator} from './logger.js';
+
+class BankAccount {
+  static bankName = 'Gabriel Bank';
+  static #totalAccounts = 0;
+
+  constructor(owner, initialBalance = 0) {
+    this.owner = owner;
+    this._balance = initialBalance;
+    this.createdAt = new Date();
+    BankAccount.#totalAccounts++;
+  }
+
+  get balance() {
+    return this._balance;
+  }
+
+  deposit(amount) {
+    if (amount <= 0) throw new Error('Invalid amount');
+    this._balance += amount;
+    return this;
+  }
+
+  withdraw(amount) {
+    if (amount > this._balance) throw new Error("You don't have money!");
+    this._balance -= amount;
+    return this;
+  }
+
+  transfer(amount, targetAccount) {
+    this.withdraw(amount);
+    targetAccount.deposit(amount);
+    return this;
+  }
+
+  toString() {
+    return `${this.owner}'s account: $${this._balance}`;
+  }
+
+  static createJointAccount(owner1, owner2, initialBalance) {
+    return new BankAccount(`${owner1} & ${owner2}`, initialBalance);
+  }
+
+  static getAccountCount() {
+    return BankAccount.#totalAccounts;
+  }
+}
+
+const gabriel = new BankAccount('Gabriel', 1000);
+const fernanda = new BankAccount('Fernanda', 500);
+
+console.log({deposit: gabriel.deposit(200).withdraw(50)});
+console.log({transfer: gabriel.transfer(100, fernanda)});
+
+console.log(gabriel.balance);
+console.log(fernanda.balance);
+console.log(String(gabriel));
+
+console.log({getAccountCount: BankAccount.getAccountCount()});
+console.log({bankName: BankAccount.bankName});
+
+separator();
