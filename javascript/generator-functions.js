bc9401ede43300163e5a69fa91d733f86cca2069
Gabriel Debona <gabriel.debona@gmail.com>
2026-02-02 09:42:59 -0300

    Created generator function POC

--- javascript/generator-functions.js
+++ javascript/generator-functions.js
@@ -2,8 +2,23 @@ import {heading} from "logger.js";
 
 heading('Generator Functions');
 
+/* --- valid ways to create --- */
+
+function* gen1() {}
+function *gen2() {}
+function * gen3() {}
+
+/* ----------- Code ------------ */
+
 function* generatorFunction() {
   yield 1;
   yield 2;
   yield 3;
 }
+
+const generator = generatorFunction();
+
+console.log({generator: generator.next()});
+console.log({generator: generator.next()});
+console.log({generator: generator.next()});
+console.log({generator: generator.next()});
