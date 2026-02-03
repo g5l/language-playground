d17db4575c55117553396104610d35bbef828066
Gabriel Debona <gabriel.debona@gmail.com>
2026-02-03 09:50:16 -0300

    Improved Generator function POC

--- javascript/generator-functions.js
+++ javascript/generator-functions.js
@@ -1,39 +1,38 @@
-import {heading} from "logger.js";
+import {heading, subTitle} from "./logger.js";
 
 heading('Generator Functions');
 
 /* ----- Syntax variation ----- */
 
-function* myGenerator() {
-  yield 'a';
-}
-
-const myGenerator = function* () {
-  yield 'a';
-};
-
-const myGenerator = function* namedGenerator() {
-  yield 'a';
-};
-
-const obj = {
-  *myGenerator() {
-    yield 'a';
-  }
-};
-
-class MyClass {
-  *myGenerator() {
-    yield 'a';
-  }
-}
-
+// function* myGenerator() {
+//   yield 'a';
+// }
+//
+// const myGenerator = function* () {
+//   yield 'a';
+// };
+//
+// const myGenerator = function* namedGenerator() {
+//   yield 'a';
+// };
+//
+// const obj = {
+//   *myGenerator() {
+//     yield 'a';
+//   }
+// };
+//
+// class MyClass {
+//   *myGenerator() {
+//     yield 'a';
+//   }
+// }
 
 /* --- valid ways to create --- */
 
-function* gen1() {}
-function *gen2() {}
-function * gen3() {}
+// function* gen1() {}
+// function *gen2() {}
+// function * gen3() {}
 
 /* ----------- Code ------------ */
 
@@ -49,3 +48,17 @@ console.log({generator: generator.next()});
 console.log({generator: generator.next()});
 console.log({generator: generator.next()});
 console.log({generator: generator.next()});
+
+subTitle('Key world yield');
+
+function* demo() {
+  yield 1;
+
+  yield 2;
+
+  const x = yield 3;
+
+  return 'done';
+}
+
+console.log({yield: [...demo()]})
